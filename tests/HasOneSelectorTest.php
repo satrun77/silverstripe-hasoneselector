3aefa6b2b8b73009fdc9e39b05cbae68310f86f8
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2018-07-11 00:19:36 +1200

    Test: added selecting/removing item test case

--- tests/HasOneSelectorTest.php
+++ tests/HasOneSelectorTest.php
@@ -31,6 +31,29 @@ class HasOneSelectorTest extends FunctionalTest
         $this->assertEquals($resource->Title, $field->getColumnContent($field->getRecord(), 'Title'));
     }
 
+    public function testSelectingRemovingItem()
+    {
+        $page     = $this->getPage('page-1');
+        $field    = $this->getField($page);
+
+        $resource = $this->getResource('resource-1');
+        $field->getList()->add($resource);
+        $form = new Form(Controller::curr(), 'Form', FieldList::create($field), FieldList::create());
+
+        $this->assertInstanceOf(ResourceTest::class, $field->getRecord());
+        $this->assertInstanceOf(PageTest::class, $field->getOwner());
+        $this->assertEquals($field->getList()->first()->ID, $resource->ID);
+        $this->assertEquals($field->getList(), $field->getManipulatedList());
+        $this->assertEquals($resource->Title, $field->getColumnContent($field->getRecord(), 'Title'));
+
+        $field->getList()->remove($resource);
+        $html  = $field->FieldHolder();
+
+        $this->assertContains('No resource test selected', $html->RAW());
+        $this->assertEquals(ResourceTest::class, $field->getDataClass());
+        $this->assertFalse($field->getRecord()->exists());
+    }
+
     /**
      * @param $name
      * @return PageTest
